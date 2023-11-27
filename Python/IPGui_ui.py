adeec28a50337887b69cb7b650d405eef75b684a
Robert Brenner Marthins <04roberten@gmail.com>
2023-11-27 14:35:04 +0100

    ip config is added as dialog to main

---
+++ Python/IPGui_ui.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'IPGui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QHBoxLayout,
+    QLabel, QListWidget, QListWidgetItem, QPushButton,
+    QSizePolicy, QSpinBox, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(787, 496)
+        Form.setStyleSheet(u"font: 22pt \"Segoe UI\";")
+        self.layoutWidget = QWidget(Form)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(260, 60, 401, 46))
+        self.horizontalLayout = QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.spinEdit1 = QSpinBox(self.layoutWidget)
+        self.spinEdit1.setObjectName(u"spinEdit1")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.spinEdit1.sizePolicy().hasHeightForWidth())
+        self.spinEdit1.setSizePolicy(sizePolicy)
+        self.spinEdit1.setMinimumSize(QSize(45, 0))
+        self.spinEdit1.setStyleSheet(u"")
+        self.spinEdit1.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinEdit1.setMinimum(0)
+        self.spinEdit1.setMaximum(255)
+        self.spinEdit1.setValue(192)
+
+        self.horizontalLayout.addWidget(self.spinEdit1)
+
+        self.label_4 = QLabel(self.layoutWidget)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout.addWidget(self.label_4)
+
+        self.spinEdit2 = QSpinBox(self.layoutWidget)
+        self.spinEdit2.setObjectName(u"spinEdit2")
+        self.spinEdit2.setMinimumSize(QSize(40, 0))
+        self.spinEdit2.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinEdit2.setMinimum(0)
+        self.spinEdit2.setMaximum(255)
+        self.spinEdit2.setValue(168)
+
+        self.horizontalLayout.addWidget(self.spinEdit2)
+
+        self.label_5 = QLabel(self.layoutWidget)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout.addWidget(self.label_5)
+
+        self.comboBox = QComboBox(self.layoutWidget)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy1)
+        self.comboBox.setMinimumSize(QSize(60, 0))
+
+        self.horizontalLayout.addWidget(self.comboBox)
+
+        self.label_6 = QLabel(self.layoutWidget)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout.addWidget(self.label_6)
+
+        self.spinBox1 = QSpinBox(self.layoutWidget)
+        self.spinBox1.setObjectName(u"spinBox1")
+        self.spinBox1.setMinimum(0)
+        self.spinBox1.setMaximum(2)
+        self.spinBox1.setStepType(QAbstractSpinBox.DefaultStepType)
+        self.spinBox1.setValue(1)
+
+        self.horizontalLayout.addWidget(self.spinBox1)
+
+        self.spinBox2 = QSpinBox(self.layoutWidget)
+        self.spinBox2.setObjectName(u"spinBox2")
+        self.spinBox2.setMinimum(0)
+        self.spinBox2.setMaximum(9)
+        self.spinBox2.setValue(9)
+
+        self.horizontalLayout.addWidget(self.spinBox2)
+
+        self.spinBox3 = QSpinBox(self.layoutWidget)
+        self.spinBox3.setObjectName(u"spinBox3")
+        self.spinBox3.setMinimum(0)
+        self.spinBox3.setMaximum(9)
+        self.spinBox3.setValue(2)
+
+        self.horizontalLayout.addWidget(self.spinBox3)
+
+        self.checkButton = QPushButton(Form)
+        self.checkButton.setObjectName(u"checkButton")
+        self.checkButton.setGeometry(QRect(370, 220, 121, 71))
+        self.listWidget = QListWidget(Form)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setEnabled(True)
+        self.listWidget.setGeometry(QRect(30, 270, 256, 192))
+        self.listHeader = QLabel(Form)
+        self.listHeader.setObjectName(u"listHeader")
+        self.listHeader.setEnabled(True)
+        self.listHeader.setGeometry(QRect(70, 240, 201, 31))
+        self.listHeader.setStyleSheet(u"font: 13pt \"Segoe UI\";")
+        self.info_text = QLabel(Form)
+        self.info_text.setObjectName(u"info_text")
+        self.info_text.setGeometry(QRect(40, 120, 721, 51))
+        self.info_text.setStyleSheet(u"color: rgb(255, 0, 0);\n"
+"text-align: center;")
+
+        self.retranslateUi(Form)
+
+        self.comboBox.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u".", None))
+        self.label_5.setText(QCoreApplication.translate("Form", u".", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("Form", u"4", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("Form", u"10", None))
+
+        self.label_6.setText(QCoreApplication.translate("Form", u".", None))
+        self.checkButton.setText(QCoreApplication.translate("Form", u"Check", None))
+        self.listHeader.setText(QCoreApplication.translate("Form", u"Attempted ip addresses", None))
+        self.info_text.setText("")
+    # retranslateUi
+
--- Python/Remote.py
+++ Python/Remote.py
@@ -1,9 +1,139 @@
-import sys
-from IPconfigurator import IPconfigGUI
-from PySide6.QtWidgets import QApplication
+import sys, cv2
+from PySide6.QtCore import QThread, Signal, Slot, Qt
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QListWidgetItem
 
-app = QApplication(sys.argv)
-IPGui = IPconfigGUI.MainWindow()
-IPGui.show()
+from application_ui import Ui_MainWindow
+from IPGui_ui import Ui_Form
 
-sys.exit(app.exec())
+class VideoThread(QThread):
+    change_pixmap_signal = Signal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                h, w, ch = rgb_image.shape
+                bytes_per_line = ch * w
+                qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                self.change_pixmap_signal.emit(qt_image)
+
+class IPConfigWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.listHeader.setVisible(False)
+        self.ui.listWidget.setVisible(False)
+
+        self.ip_address = ""
+
+        self.spinBoxNames = ["spinBox1", "spinBox2", "spinBox3"]
+
+        for name in self.spinBoxNames:
+            self.ui.__dict__[name].valueChanged.connect(self.handle_spinBox_value)
+
+        self.ui.checkButton.clicked.connect(self.connect)
+
+    def update_spinBox_max(self, names, max_value):
+        for name in names:
+            spin_box = self.ui.__dict__[name]
+
+            # Disconnect valueChanged signal temporarily
+            spin_box.valueChanged.disconnect(self.handle_spinBox_value)
+
+            spin_box.setMaximum(max_value)
+
+            # Reconnect valueChanged signal
+            spin_box.valueChanged.connect(self.handle_spinBox_value)
+
+    def handle_spinBox_value(self):
+        spin_values = []
+        for name in self.spinBoxNames:
+            spin_box = self.ui.__dict__[name]
+            spin_values.append(spin_box.value())
+        if spin_values[0] == 2:
+            self.update_spinBox_max([self.spinBoxNames[1],self.spinBoxNames[2]], 5)
+            spin_values[1] = min(spin_values[1], 5)
+            spin_values[2] = min(spin_values[2], 5)
+
+        else:
+            self.update_spinBox_max([self.spinBoxNames[1],self.spinBoxNames[2]], 9)
+
+
+    def connect(self):
+        spin_values = [self.ui.__dict__[name].value() for name in self.spinBoxNames]
+        combo_value = self.ui.comboBox.currentText()
+        line_edit_values = [self.ui.spinEdit1.text(), self.ui.spinEdit2.text()]
+
+        self.ip_address = ".".join(line_edit_values) + "." + combo_value + "." + "".join(map(str, spin_values))
+        
+        print(self.ip_address)
+        self.failed_connect()
+
+    def failed_connect(self):
+        self.ui.listHeader.setVisible(True)
+        self.ui.listWidget.setVisible(True)
+        self.ui.info_text.setText(f"Error {self.ip_address}")
+
+        # Create a custom widget for each item in the list
+        item_widget = QWidget()
+        item_layout = QVBoxLayout(item_widget)
+
+        # Label for displaying the IP address
+        ip_label = QPushButton(self.ip_address)
+        
+        ip_label.setStyleSheet("font-size: 15pt;") # Adjust the size as needed
+        
+        ip_label.clicked.connect(self.handle_ip_label_click)
+        item_layout.addWidget(ip_label)
+
+        # Create a list widget item and set the widget for it
+        listWidgetItem = QListWidgetItem()
+        listWidgetItem.setSizeHint(item_widget.sizeHint())
+        
+        # Insert the item at the top of the list
+        self.ui.listWidget.insertItem(0, listWidgetItem)
+        self.ui.listWidget.setItemWidget(listWidgetItem, item_widget)
+
+
+    def handle_ip_label_click(self):
+        sender_button = self.sender()
+        if sender_button is not None:
+            ip_address = sender_button.text()
+            self.reconnect(ip_address)
+
+    def reconnect(self, ip_address):
+        print(f"Reconnecting to {ip_address}")
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
+        self.video_thread = VideoThread()
+        self.video_thread.change_pixmap_signal.connect(self.update_video_frame)
+        self.video_thread.start()
+        self.show_ip_dialog()
+    
+    def show_ip_dialog(self):
+        dialog = IPConfigWindow(self)
+        
+
+    @Slot(QImage)
+    def update_video_frame(self, image):
+        label_width = self.ui.videoFrame.width()
+        label_height = self.ui.videoFrame.height()
+        pixmap = QPixmap.fromImage(image).scaled(label_width, label_height, Qt.KeepAspectRatio)
+        self.ui.videoFrame.setPixmap(pixmap)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
